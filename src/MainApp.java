d91bae9138990ddc7043da9f83a2447beeb059dc
Kristen Carethers <kcarethers@gmail.com>
2016-09-29 16:46:17 -0400

    Did while loop structure asking if user wants to check out

--- src/MainApp.java
+++ src/MainApp.java
@@ -9,6 +9,9 @@ public class MainApp {
 		System.out.println("Welcome to PopUpPup!!! Please browse our menu: ");
 		
 		System.out.println();
+		//while loop variables
+		String cont;
+		cont = "y";
 		
 		Scanner sc = new Scanner(System.in); 
 		
@@ -24,9 +27,30 @@ public class MainApp {
 		Products.add(new Product("Puppy Delight", "Puppy food", 5.00));
 		Products.add(new Product("Delightful Treats", "Treats", 3.00));
 		Products.add(new Product("Good Bone", "Dog bone", 2.00));
-		Products.add(new Product("Blush Winter Coat","Red" ));
-		Products.add(new Product("Winter Coat ")
+		Products.add(new Product("Blush Winter Coat","Red Sweater", 20.00 ));
+		Products.add(new Product("Pale Winter Coat", "Pink Sweater", 20.00));
+		Products.add(new Product("Grass Winter Coat","Green Sweater", 20.00));
+		Products.add(new Product("Night Winter Coat", "Black Sweater", 20.00));
+		Products.add(new Products("Daisy Winter Coat", "Yellow Sweater", 20.00));
+		Products.add(new Products("Citrus Winter", "Orange Sweater", 20.00));
 		
+		while (cont.equalsIgnoreCase("y")) {
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			//Ask to check out 
+			System.out.println("Check out? (y/n):");
+			cont = input.nextLine();
+			cont = input.nextLine();
+			
+		}//while loop close
 		
 		
 		
